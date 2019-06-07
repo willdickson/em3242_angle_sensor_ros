7c3686de2f1ff3d78f2d57ac511d266911e5e984
Will Dickson <wbd@caltech.edu>
2019-06-07 15:34:10 -0700

    Modified so that filtering criteria uses max speed rather than max_step size.  Prevents it from getting stuck.  Added params to launch file.

--- nodes/em3242_angle_sensor_node.py
+++ nodes/em3242_angle_sensor_node.py
@@ -14,6 +14,7 @@ class EM3242_AngleSensorNode(object):
     def __init__(self):
         self.is_first = True
         self.last_angle = 0.0
+        self.last_t = 0.0
         self.cumulative_angle = 0.0
 
         rospy.init_node('em3242_angle_sensor')
@@ -21,28 +22,35 @@ class EM3242_AngleSensorNode(object):
         self.data_publisher = rospy.Publisher('em3242_angle_sensor_data', EM3242_AngleSensorData, queue_size=10) 
 
         self.port = rospy.get_param('/em3242_angle_sensor/port', '/dev/ttyACM0')
+        self.max_speed = rospy.get_param('/em3242_angle_sensor/max_speed', 2000.0)
         self.em3242 = EM3242_AngleSensor(self.port,sigint_handler=False)
         self.em3242.start(callback=self.on_em3242_data)
 
 
-    def update_cumulative_angle(self,angle):
+    def update_cumulative_angle(self,t,angle):
         if self.is_first:
             self.is_first = False
             self.last_angle = angle
+            self.last_t = t
             self.cumulative_angle = angle
         else:
             step = get_cumulative_step(angle, self.last_angle)
-            if abs(cum_step) < max_step:
+            dt = t - self.last_t
+            speed = abs(step/dt)
+            if speed  < self.max_speed:
                 self.last_angle = angle
+                self.last_t = t
             else:
                 step = 0.0
             self.cumulative_angle += step
 
 
     def on_em3242_data(self,angle):
-        self.update_cumulative_angle(angle)
+        t_ros = rospy.Time.now()
+        t_sec = t_ros.to_sec()
+        self.update_cumulative_angle(t_sec,angle)
         msg_data = EM3242_AngleSensorData()
-        msg_data.header.stamp = rospy.Time.now()
+        msg_data.header.stamp = t_ros
         msg_data.angle = angle
         msg_data.cumulative_angle = self.cumulative_angle
         self.data_publisher.publish(msg_data)
